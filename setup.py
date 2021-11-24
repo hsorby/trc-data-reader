f886de3f55db33ef11bea618a7f6d3393e45f742
hsorby <h.sorby@auckland.ac.nz>
2021-11-25 10:36:44 +1300

    Set version to 0.2.0.

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='trc-data-reader',
-    version='0.1.5',
+    version='0.2.0',
     py_modules=['trc'],
     package_dir={'': 'src'},
     url='https://github.com/hsorby/trc-data-reader',
     license='Apache 2.0',
     author='Hugh Sorby',
     author_email='h.sorby@auckland.ac.nz',
-    description='A package for reading trc motion capture data.',
+    description='A package for reading track row column (trc) motion capture data.',
     long_description=long_description,
     long_description_content_type='text/x-rst'
 )
