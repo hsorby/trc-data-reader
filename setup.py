5d407fd2cdfdda90fc4092f951532eae7becccb5
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-04-04 16:55:36 +0100

    Set version to 0.3.0.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = readfile("requirements.txt")
 
 setup(
     name='trc-data-reader',
-    version='0.2.0',
+    version='0.3.0',
     py_modules=['trc'],
     package_dir={'': 'src'},
     url='https://github.com/hsorby/trc-data-reader',
